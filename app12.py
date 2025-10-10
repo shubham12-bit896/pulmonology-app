375fbca4b438ba974810adafbb55b8f6ba395f9f
shubham12-bit896 <shubhamkatore2530@gmail.com>
2025-10-10 15:32:48 +0530

    Add files via upload

--- app12.py
+++ app12.py
@@ -99,7 +99,7 @@ class Role:
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     role = db.Column(db.String(50), nullable=False, default=Role.HEALTH_WORKER)
     def set_password(self, password): self.password_hash = generate_password_hash(password)
     def check_password(self, password): return check_password_hash(self.password_hash, password)
